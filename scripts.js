611404e7eead57113f8fd56d06210250a65bf461
Ersin ÇETİN <ersin.cetin@hotmail.com>
2020-06-04 13:18:55 +0300

    Update

--- scripts.js
+++ scripts.js
@@ -4,37 +4,36 @@ let currentYear = today.getFullYear();
 let months = ["Oca", "Şub", "Mar", "Nis", "May", "Haz", "Tem", "Ağu", "Eyl", "Eki", "Kas", "Ara"];
 let days = ['Paz', 'Pzt', 'Sal', 'Çar', 'Per', 'Cum', 'Cmt'];
 let font_type = ['fa-check', 'fa-percent', 'fa-cog', 'fa-exclamation', 'fa-times'];
-let font_color = ['#00c851','#00c851','#ffb54c','#ffb54c', '#ff4444' ];
-fullYear(currentMonth,currentYear);
-function fullYear(month,year) {
+let font_color = ['#00c851', '#00c851', '#ffb54c', '#ffb54c', '#ff4444'];
+fullYear(currentMonth, currentYear);
+
+function fullYear(month, year) {
     for (let i = 0; i < months.length; i++) {
-        showCalendar(i,year);
+        showCalendar(i, year);
     }
 }
+
 function showCalendar(month, year) {
     let firstDay = (new Date(year, month)).getDay();
     let daysInMonth = 32 - new Date(year, month, 32).getDate();
     let tbl = document.getElementById("calendar-body");
     let row = document.createElement("tr");
     let cell = document.createElement("th");
-    let cellText = document.createTextNode(months[month]+" / "+currentYear);
+    let cellText = document.createTextNode(months[month] + " / " + currentYear);
     cell.appendChild(cellText);
     row.appendChild(cell);
     for (let j = 0; j < 31; j++) {
-        if (j+1 > daysInMonth){
+        if (j + 1 > daysInMonth) {
             break;
         }
         let cell = document.createElement("td");
-        let br = document.createElement("br");
-        let cellText = document.createTextNode(days[(firstDay+j)%7]);
-        let font = document.createElement("i");
-        let i = Math.floor(Math.random() * 5);
-        font.style.color="#00c851";
-        font.classList.add("fa");
-        font.classList.add("fa-check");
+        if ((firstDay + j) % 7 == 0 || (firstDay + j) % 7 == 6) {
+            cell.style.backgroundColor = "#ffb54c";
+        } else {
+            cell.style.backgroundColor = "#00c851";
+        }
+        let cellText = document.createTextNode(days[(firstDay + j) % 7]);
         cell.appendChild(cellText);
-        cell.appendChild(br);
-        cell.appendChild(font);
         row.appendChild(cell);
     }
     tbl.appendChild(row);
